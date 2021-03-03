6f0036da58c8c495be9f0cf5b36cafe754996f0d
lnabua <nabua.lorraine@gmail.com>
2021-03-02 23:08:01 -0800

    commit tree dac64a47f8da360fcb5ff7e9afaaf8c0b36ec71d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+// find years won in superbowl and return years won
+function superbowlWin(record) {
+    const recordData = record.find((data) => data.result === "W");
+    if (recordData != undefined) {
+        return recordData.year;
+    } else {
+        return undefined;
+    }
+}
 
+/* Using iterator method
+function yearWon(data) {
+    for (const user of data) {
+        if (user.result === "W") {
+            console.log(user.year);
+        }
+    }
+}
+yearWon(record);
+*/
